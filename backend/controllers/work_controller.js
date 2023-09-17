8e850316435acac44e72360f9583a69bee97a3c4
aar121 <arifrahim12345@gmail.com>
2023-09-17 14:51:42 -0400

    added mock schema data and edited route

--- backend/controllers/work_controller.js
+++ backend/controllers/work_controller.js
@@ -1,16 +1,53 @@
 const express = require("express");
 const router = require("express").Router();
 require("dotenv").config();
-const db = require('../models')
+const db = require('../models/job.js');
+//const Job = require("../models/data");
+const Jobs = [{name: "Best Buy",
+  contact: "Email",
+  title: "Cashier",
+  location: "New York"}, {name: "Nike",
+  contact: "Email",
+  title: "Sales",
+  location: "Chicago"}
+]
 
-router.get('/', (req, res) => {
-    db.Job.find()
-    .then((job) => {
-      res.render('places/more', { job })
-    })
-    .catch(err => {
-      console.log(err)
-      res.render('error404')
-    })
-  })
-  
+router.get('/', async (req, res) => {
+  // const Jobs = await Job.findAll()
+  //res.json(places)
+  res.json(Jobs)
+})
+
+// router.post('/', (req, res) => {
+//   db.Job.create (req.body)
+//   .then((job) => {
+//     res.status(201).json(job)
+//   })
+//   .catch(err => {
+//     if (err && err.name == 'ValidationError') {
+//       let message = 'Validation Error: '
+//       for (var field in err.errors) {
+//         message += `${field} was ${err.errors[field].value}`
+//         message += `${err.errors[field].message}`
+//       }
+//       console.log('Validation error message', message)
+//       res.status(401).json({ message, prev: req.body })
+//     } else {
+//       console.log(err)
+//       res.status(500).send("Internal error")
+//     }
+//   })
+// })
+
+// router.put('/:id', (req, res) => {
+//   db.Job.findByIdAndUpdate(req.params.id, req.body, { runValidators: true }).
+//   then(job => {
+//     res.redirect(`/api/places/${req.params.id}`)
+//   })
+//   .catch(err=>{
+//       console.log(err) 
+//       res.status(401).json(err)
+//   })
+// })
+
+module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log ('hello world');
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+
+// Express Settings
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Controllers & Routes
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/jobs', require('./controllers/work_controller'))
+//app.use('/users', require('./work_controllers/users'))
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+})
