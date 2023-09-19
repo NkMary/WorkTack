4c2e11de9da79d9673dbef2f7fb3f13e1178aa3d
aar121 <arifrahim12345@gmail.com>
2023-09-19 19:29:52 -0400

    import usestate usenavigate

---
+++ frontend/src/components/places/Add.jsx
@@ -0,0 +1,58 @@
+const React = require("react");
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+//const NavBar = require('../NavBar.jsx')
+
+function AddJob() {
+  const navigate = useNavigate();
+
+  const [job, setJob] = useState({
+    name: "",
+    title: "",
+    location: "",
+    contact: "",
+  });
+  return (
+    <main>
+      <h1>Add a Job Listing</h1>
+      <form method="POST" action="/places">
+        <div className="form-group">
+          <label htmlFor="name">Business Name</label>
+          <input
+            className="form-control"
+            id="name"
+            name="name"
+            type="text"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="title">Job Title</label>
+          <input
+            className="form-control"
+            id="title"
+            name="title"
+            type="text"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input
+            className="form-control"
+            id="location"
+            name="location"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="contact">Contact Info</label>
+          <input className="form-control" id="contact" name="contact" />
+        </div>
+        <input className="btn btn-primary" type="submit" value="Add Job" />
+      </form>
+    </main>
+  );
+}
+
+export default AddJob;
